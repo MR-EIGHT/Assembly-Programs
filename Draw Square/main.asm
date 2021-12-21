57db9f43da109c668a7fc580a6d7c9f0422a9ef1
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-12-21 11:13:13 +0330

    comments improved.

--- Draw Square/main.asm
+++ Draw Square/main.asm
@@ -5,31 +5,43 @@ dosseg
 
 
 .CODE
+
 MAIN PROC
+
+; enter video mode
+
 mov ah, 0   ; set display mode function.
 mov al, 13h ; mode 13h = 320x200 pixels, 256 colors.
-int 10h     ; set it!
+int 10h     ; set it by calling the interrupt.
 
-mov cx, 30  ;col
-mov dx, 30  ;row
-mov ax, 44h ;44h is yellow
+; setting row, column, colour and interrupt
+
+mov cx, 30  ; col => x
+mov dx, 30  ; row => y
+mov ax, 44h ; 44h is yellow
 mov ah, 0ch ; put pixel
 
-colcount:
-inc cx
+; fill a row
+
+columns:
+inc cx	
 int 10h
 cmp cx,60
-jb colcount
+jb columns
+
+; next row
 
 mov cx, 30  ; reset to start of col
-inc dx      ;next row
+inc dx      ; next row
 cmp dx, 60
-JNE colcount
+JNE columns
+
 
 ; exit
 
-mov ax,4C00h  ;terminate program
+mov ax,4C00h  ; terminate program
 int 21h
+
 
 ;exiting video-mode and entering text mode:
 	;mov ax,0003h
